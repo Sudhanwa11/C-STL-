29320f2742cd216322bda966b5b6bb3d96f068d0
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-04-16 20:03:40 +0530

    Create STACKSTL.cpp

---
+++ STACK/STACKSTL.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int main() {
+    //to create stack
+    stack<string> s;
+    
+    //to add elements
+    s.push("love");
+    s.push("sudhanwa");
+    s.push("vaid");
+    
+    //to find top element
+    cout<< "top element is "<<s.top()<<endl;
+    cout<<endl;
+    
+    //to remove element
+    s.pop();
+    cout<< "now top element is "<<s.top()<<endl;
+    cout<<endl;
+    
+    //to find size
+    cout<< "size of stack "<<s.size()<<endl;
+}
