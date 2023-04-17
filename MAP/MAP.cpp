275c0d597d2bd54bffa2de4e4dcfeb8afafcf561
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-04-17 20:19:22 +0530

    Create MAP.cpp

---
+++ MAP/MAP.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <map>
+using namespace std;
+int main() {
+    map<int,string> m;
+    m[1] = "sudhanwa";
+    m[2] = "vaid";
+    m[13] = "love";
+    m.insert({5,"you"});
+    cout<< "before erase"<<endl;
+    for (auto i:m) {
+        cout<<i.first<<" "<<i.second<<endl;
+    }
+    cout<<endl;
+    cout<< "finding 13-> "<<m.count(13)<<endl;
+    cout<<endl;
+    
+    m.erase(13);
+    cout<< "after erase "<<endl;
+    for (auto i:m) {
+        cout<<i.first<<" "<<i.second<<endl;
+    }
+    cout<<endl;
+    
+    auto it = m.find(5);
+    for (auto i=it; i!=m.end(); i++) {
+        cout<<(*i).first<<endl;
+    }
+}
