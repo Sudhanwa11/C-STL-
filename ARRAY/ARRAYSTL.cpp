04d2aa579142c04fa11f3722916c950a712825df
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-06-03 17:34:23 +0530

    Update ARRAYSTL.cpp

--- ARRAY/ARRAYSTL.cpp
+++ ARRAY/ARRAYSTL.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 int main() {
-
     array <int,4> a = {1,2,3,4};
     int size = a.size();
     for(int i = 0; i<size; i++) {
         cout<< a[i]<<endl;
     }
+
 
     //to find particular element at an index
     cout<< "element at second " << a.at(2) <<endl;
