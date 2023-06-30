42897ce795ad0df59891dfde4ded0c6f776ac280
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-06-30 21:01:44 +0530

    Create convert to binary.cpp

---
+++ Convert to binary/convert to binary.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+string toBinary(int n, int len)
+{
+    string binary;
+    for (unsigned i = (1 << len - 1); i > 0; i = i / 2) {
+        binary += (n & i) ? "1" : "0";
+    }
+ 
+    return binary;
+}
+ 
+int main()
+{
+    int n = 2;
+    int len = 32;
+ 
+    cout << "The binary representation of " << n << " is " << toBinary(n, len);
+ 
+    return 0;
+}
