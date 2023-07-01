02f1cefd78d67301dd4320bbea3e43df78a45735
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-07-01 16:50:28 +0530

    Update convert to binary.cpp

--- Convert to binary/convert to binary.cpp
+++ Convert to binary/convert to binary.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
-string toBinary(int n, int len)
-{
+
+string toBinary(int n, int len) {
     string binary;
     for (unsigned i = (1 << len - 1); i > 0; i = i / 2) {
         binary += (n & i) ? "1" : "0";
