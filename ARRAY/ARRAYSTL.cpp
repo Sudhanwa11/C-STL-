3a6befdbd6b0d596509013d716a85612b7e6bfce
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-08-10 20:33:04 +0530

    Update ARRAYSTL.cpp

--- ARRAY/ARRAYSTL.cpp
+++ ARRAY/ARRAYSTL.cpp
@@ -8,8 +8,6 @@ int main() {
     for(int i = 0; i<size; i++) {
         cout<< a[i]<<endl;
     }
-
-
     //to find particular element at an index
     cout<< "element at second " << a.at(2) <<endl;
     //to check whether an array is empty
